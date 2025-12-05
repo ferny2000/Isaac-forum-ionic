daa39591a6d3b01c387372fe7e1ecd9ebb63a2ad
ferny2000 <ferpadilolxd@gmail.com>
2025-12-05 17:26:27 -0600

    z

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,10 @@
 const mysql = require('mysql2/promise');
 
-// Define la configuración basándote en si existe MYSQL_URL o no
-const dbConfig = process.env.MYSQL_URL || {
+const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
-  port: process.env.PORT || 3306 // Asegura el puerto si es local
-};
-
-const pool = mysql.createPool({
-  ...((typeof dbConfig === 'string') ? { uri: dbConfig } : dbConfig), // Si es string usa uri, si es objeto lo expande
   waitForConnections: true,
   connectionLimit: 10,
 });
