30a5bbc898166540ad3201706ccb804e60c931b5
ferny2000 <ferpadilolxd@gmail.com>
2025-12-05 18:37:48 -0600

    Revert "z"
    This reverts commit daa39591a6d3b01c387372fe7e1ecd9ebb63a2ad.

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
+// Define la configuración basándote en si existe MYSQL_URL o no
+const dbConfig = process.env.MYSQL_URL || {
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
+  port: process.env.PORT || 3306 // Asegura el puerto si es local
+};
+
+const pool = mysql.createPool({
+  ...((typeof dbConfig === 'string') ? { uri: dbConfig } : dbConfig), // Si es string usa uri, si es objeto lo expande
   waitForConnections: true,
   connectionLimit: 10,
 });
